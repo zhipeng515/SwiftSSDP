548dd94af04754daab038f9090db41518e95bdc7
Tim Carr <xaphod@gmail.com>
2020-05-04 12:22:50 -0400

    Fix compile error: bump min iOS version from 9 to 10

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftSSDP",
     platforms: [
         .macOS(.v10_12),
-        .iOS(.v9)
+        .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
